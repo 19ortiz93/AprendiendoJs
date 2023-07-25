526510cd87ff72b2d37d8c144b03a1b60b1fda12
Fernando Ortiz <101433810+19ortiz93@users.noreply.github.com>
2023-07-25 15:19:15 -0500

    NodeJs
    Curso básico de Nodejs

---
+++ NodeJS/index1.js
@@ -0,0 +1,9 @@
+const math = require('./math1.js');
+
+console.log(math);
+
+/*console.log(math.add(3, 5));
+console.log(math.substract(3, 5));
+console.log(math.multiply(3, 5));
+console.log(math.divide(9, 3));
+console.log(math.divide(3, 0));*/
